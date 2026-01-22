3028966c527ecdca4e7df31ce2c843f6708fb7d2
raihanshi18 <raihanshiddiq18@gmail.com>
2026-01-22 09:02:14 +0700

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const app = express();
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
+const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
+
 app.use(express.json());
 
 const productController = require("./product/product.controller");
 const categoryController = require("./Categories/category.controller");
+
+const corsOption = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOption));
 
 app.use("/products", productController);
 app.use("/categories", categoryController);
